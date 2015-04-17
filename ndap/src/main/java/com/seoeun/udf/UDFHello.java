2e70a49a81bb70b0c4de9d8d248950c148ddd168
Azrael Park <azrael.park@nexr.com>
2015-04-17 10:00:21 +0900

    Add udf 'addhello'

--- ndap/src/main/java/com/seoeun/udf/UDFHello.java
+++ ndap/src/main/java/com/seoeun/udf/UDFHello.java
@@ -1,7 +1,13 @@
 package com.seoeun.udf;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
 
 public class UDFHello extends UDF{
 
+    public Text evaluate(Text input) {
+        String a = input.toString();
+        return new Text(a + "-hello");
+    }
 }
