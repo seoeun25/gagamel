edb741c30d3c38e469a9920cc7ec26682364f2a6
Azrael Park <azrael.park@nexr.com>
2015-04-17 10:00:58 +0900

    Add description to addhello

--- ndap/src/main/java/com/seoeun/udf/UDFHello.java
+++ ndap/src/main/java/com/seoeun/udf/UDFHello.java
@@ -1,9 +1,14 @@
 package com.seoeun.udf;
 
+import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
 
-
+@Description(name = "addhello",
+        value = "_FUNC_(str) -  Add 'hello' to str.\n"
+                + "\nExample:\n"
+                + "> SELECT _FUNC_('abc') FROM src; returns 'str-hello'\n"
+)
 public class UDFHello extends UDF{
 
     public Text evaluate(Text input) {
