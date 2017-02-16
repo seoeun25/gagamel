02bcb6023ec67b0a6d5b4da6f4396b7f52aed2df
azrael.seoeun <seoeun25@gmail.com>
2017-02-16 23:53:20 +0900

    test

--- repo/src/main/java/com/seoeun/server/AppService.java
+++
@@ -1,9 +0,0 @@
-package com.seoeun.server;
-
-import com.seoeun.AvroRepoException;
-
-public interface AppService {
-    public void start() throws AvroRepoException;
-
-    public void shutdown() throws AvroRepoException;
-}
